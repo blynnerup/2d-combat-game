d62e084937fc92dfb9453b65292d8296c0d7f2a6
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-04-06 07:34:24 +0200

    Base setup
    Creates basic shape for testing input system.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+}
