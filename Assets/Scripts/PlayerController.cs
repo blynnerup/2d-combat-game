0d1adde4f096ce72be9b7ded70275b2a84782a1d
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-04-06 11:38:46 +0200

    Playermovement
    Setup of player movement and adjusting values.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -6,4 +7,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls _playerControls;
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody2D;
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * (moveSpeed * Time.fixedDeltaTime));    
+    }
 }
