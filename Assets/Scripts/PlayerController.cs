8e546ffbba6cb8691a2dc7c62c1f66869332a1c3
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-04-08 17:43:53 +0200

    Adds animations and flips sprit to look at mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,23 @@ public class PlayerController : MonoBehaviour
     private PlayerControls _playerControls;
     private Vector2 _movement;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private static readonly int MoveX = Animator.StringToHash("moveX");
+    private static readonly int MoveY = Animator.StringToHash("moveY");
+    private Camera _camera;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -34,16 +47,35 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        
+        _animator.SetFloat(MoveX, _movement.x);
+        _animator.SetFloat(MoveY, _movement.y);
     }
 
     private void Move()
     {
         _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * (moveSpeed * Time.fixedDeltaTime));    
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        var playerScreenPoint = _camera.WorldToScreenPoint(transform.position);
+        var mousePos = Input.mousePosition;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;   
+        }
     }
 }
