972746f54fbc84580d2ae910890ccca5df53a3df
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-04-09 21:16:19 +0200

    Creates pathfinding for the slimes
    Also adds it as a prefab

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -1,25 +1,40 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyAi : MonoBehaviour
 {
+    
     private State _state;
     private EnemyPathfinding _enemyPathfinding;
+    private Rigidbody2D _rigidbody2D;
+    
 
     private void Awake()
     {
         _state = State.Roaming;
         _enemyPathfinding = GetComponent<EnemyPathfinding>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoaminRoutine());
     }
 
     private IEnumerator RoaminRoutine()
     {
         while (true)
         {
-            
+            Vector2 roamPosition = GetRoamingPosition();
+            _enemyPathfinding.MoveTo(roamPosition);
+            yield return new WaitForSeconds(2f);
         }
+    }
+    
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
 
     private enum State
--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 2f;
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _moveDirection;
+
+    private void Awake()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _moveDirection * (moveSpeed * Time.fixedDeltaTime));    
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        _moveDirection = targetPosition;
     }
 }
