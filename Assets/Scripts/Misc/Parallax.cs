a3575683d7080317dfd0cb2dc374ff68b369c387
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-04-13 05:13:02 +0200

    Adds parallax

---
+++ Assets/Scripts/Misc/Parallax.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxOffset = -0.15f;
+    
+    private Camera _camera;
+    private Vector2 _startPos;
+    private Vector2 _travel => (Vector2)_camera.transform.position - _startPos;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = _startPos + _travel * parallaxOffset;
+    }
+}
